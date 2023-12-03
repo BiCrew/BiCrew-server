66c8b14c79a380afeecb66ece0960878cb612581
EastHShin <xldk78@gmail.com>
2023-12-03 20:24:37 +0900

    init project

---
+++ src/main/java/com/bicrew/bicrew/BicrewApplication.java
@@ -0,0 +1,13 @@
+package com.bicrew.bicrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BicrewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bicrew/bicrew/BicrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicrew.bicrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
